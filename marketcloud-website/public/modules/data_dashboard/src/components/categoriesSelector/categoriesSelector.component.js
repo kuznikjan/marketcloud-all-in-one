17a35b839bd7a483ebae48d9188d990b527cf1e5
gradisarjoze <gradisar.joze8@gmail.com>
2019-04-30 12:55:26 +0200

    multiple category support

--- marketcloud-website/public/modules/data_dashboard/src/components/categoriesSelector/categoriesSelector.component.js
+++ marketcloud-website/public/modules/data_dashboard/src/components/categoriesSelector/categoriesSelector.component.js
@@ -10,13 +10,15 @@ angular
       enableCreateCategory: '@',
       category: '=',
       onError: '&',
-      onChange: '&'
+      onChange: '&',
+      clearInput: '&'
     }
   })
 
 CategoriesSelectorController.$inject = ['$marketcloud', '$scope', '$element']
 
 function CategoriesSelectorController($marketcloud, scope, $elem) {
+
   var ctrl = null
   this.$onInit = function() {
     ctrl = this
@@ -24,6 +26,7 @@ function CategoriesSelectorController($marketcloud, scope, $elem) {
     scope.newCategory = {}
     ctrl.modal = $($elem).find('.modal')[0];
 
+    scope.clearInput = ctrl.clearInput || false;
     scope.enableCreateCategory = ctrl.enableCreateCategory || true;
 
 
@@ -34,21 +37,13 @@ function CategoriesSelectorController($marketcloud, scope, $elem) {
       }
     }
 
-
   }
-
-
-
-
 
   scope.showModal = function() {
     $(ctrl.modal).modal('show')
   }
 
-
   function initializeData() {
-
-  
     // Initial fetch of categories
     $marketcloud.categories.list()
     .then(function(response) {
@@ -62,7 +57,7 @@ function CategoriesSelectorController($marketcloud, scope, $elem) {
       })
 
       // If found, we initialize the current category
-      if (null !== currentCategory) {
+      if (null !== currentCategory && !scope.clearInput) {
         scope.category = currentCategory;
       } else {
         // Otherwise we must fetch it
@@ -71,7 +66,7 @@ function CategoriesSelectorController($marketcloud, scope, $elem) {
 
     })
     .then(function(response){
-      if (response.data.data){
+      if (response.data.data && !scope.clearInput){
         scope.category = response.data.data;
       }
     })
@@ -83,43 +78,66 @@ function CategoriesSelectorController($marketcloud, scope, $elem) {
 
   initializeData();
 
-
   scope.setSelectedCategory = function(category) {
     scope.category = category;
     scope.ctrl.category = category.id;
     scope.ctrl.onChange({
       category: category.id
     })
+
+    /**
+     * FIXME: Handle multiple or single categories per product
+     * Currently, for sake of Epoteka integration we are saving multiple categories as a string
+     * e.g. 1523,6523,3123
+     * In future, this should be handled in an array!!!!
+     * FIXME:
+     */
+
+    console.log(scope.clearInput);
+    if (scope.clearInput) {
+      delete scope.category
+    }
+
+    /**
+     * Emiting the selected category to the parent component and handling it there
+     * TODO: Transfer entire logic to categories selector component
+     */
+
+    scope.$emit('selectedCategory', {
+      text: category.path,
+      category_id: category.id
+    });
   }
-
-
 
   // Uses the input box in the component to filter the wanted category
   scope.filterCategories = function() {
     $marketcloud.categories.list(scope.ctrl.query)
-      .then(function(response) {
+      .then(function (response) {
         ctrl.categories = response.data.data;
       })
-      .catch(function(error) {
+      .catch(function (error) {
         notie.alert(2, 'An error has occurred while fetching categories, please retry.', 2)
       })
   }
 
-
-  // Saves a new category 
+  // Saves a new category
   scope.saveCategory = function() {
     $marketcloud.categories.save(scope.newCategory)
-      .then(function(response) {
+      .then(function (response) {
         ctrl.categories.push(response.data.data)
         scope.newCategory = {}
-        ctrl.category = response.data.data.id
+
+        scope.$emit('selectedCategory', {
+          text: response.data.data.path,
+          category_id: response.data.data.id
+        });
         return $marketcloud.categories.list()
       })
-      .then(function(response) {
+      .then(function (response) {
         ctrl.categories = response.data.data
         $(ctrl.modal).modal('hide')
       })
-      .catch(function(response) {
+      .catch(function (response) {
         $(ctrl.modal).hide()
         notie.alert(3, 'An error has occurred. Category not saved', 1)
       })
