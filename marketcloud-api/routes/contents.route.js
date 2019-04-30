aa161e9452111a1e36646b34dc82dd614be8e649
gradisarjoze <gradisar.joze8@gmail.com>
2019-04-30 14:02:38 +0200

    add product to blog wip

--- marketcloud-api/routes/contents.route.js
+++ marketcloud-api/routes/contents.route.js
@@ -4,7 +4,37 @@ var Types = require('../models/types.js')
 var resource = Resource({
   singularResourceName: 'content',
   pluralResourceName: 'contents',
-  validator: Types.Content
+  validator: Types.Content,
+  hooks: {
+    afterGetById: populateProducts
+  }
 })
+
+function populateProducts (req, res, next) {
+  if (req.toSend.items && req.toSend.items.length > 0) {
+    var products = req.toSend.items.split(',')
+    var ids = products.map(function (id) {
+      return parseInt(id)
+    })
+
+    req.app.get('mongodb')
+      .collection('products')
+      .find({
+        id: {
+          $in: ids
+        }
+      })
+      .toArray(function (err, docs) {
+        if (err) {
+          throw err
+        }
+
+        req.toSend.products = docs
+        next()
+      })
+  } else {
+    next()
+  }
+}
 
 module.exports = resource.router
