35b7a734c859cfaf53aa0096c2f261d2d04fa07c
Unknown <jan.kuznik@gmail.com>
2019-06-27 12:49:31 +0200

    blog populate items update

--- marketcloud-api/routes/contents.route.js
+++ marketcloud-api/routes/contents.route.js
@@ -1,12 +1,24 @@
 var Resource = require('../libs/resource.js')
 var Types = require('../models/types.js')
 
+var Promise = require('bluebird')
+var mongoskin = require('mongoskin')
+Object.keys(mongoskin).forEach(function (key) {
+  var value = mongoskin[key]
+  if (typeof value === 'function') {
+    Promise.promisifyAll(value)
+    Promise.promisifyAll(value.prototype)
+  }
+})
+Promise.promisifyAll(mongoskin)
+
 var resource = Resource({
   singularResourceName: 'content',
   pluralResourceName: 'contents',
   validator: Types.Content,
   hooks: {
     afterGetById: populateProducts,
+    afterList: populateProductsList,
     beforeUpdate: rmemovePopulatedProperties
   }
 })
@@ -21,14 +33,29 @@ function rmemovePopulatedProperties (req, res, next) {
   next()
 }
 
+function populateProductsList (req, res, next) {
+  const promises = []
+
+  for (let i = 0; i < req.toSend.length; i++) {
+    if (req.toSend[i].items && req.toSend[i].items.length > 0) {
+      const promise = req.app.get('mongodb').collection('products').find({id: {$in: req.toSend[i].items}}).toArrayAsync()
+      promises.push(promise)
+    } else {
+      promises.push(Promise.resolve([]))
+    }
+  }
+
+  return Promise.all(promises)
+  .then((res) => {
+    for (let i = 0; i < req.toSend.length; i++) {
+      req.toSend[i].products = res[i]
+    }
+    next()
+  })
+}
+
 function populateProducts (req, res, next) {
   if (req.toSend.items && req.toSend.items.length > 0) {
-    // console.log(items);
-    // var products = req.toSend.items.split(',')
-    // var ids = products.map(function (id) {
-    //   return parseInt(id)
-    // })
-
     req.app.get('mongodb')
       .collection('products')
       .find({
